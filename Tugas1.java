42bcc466456a3bbc2a333d1b6fd4cba698d9cc2d
prilya <36910661+prilya@users.noreply.github.com>
2018-08-29 18:02:32 +0700

    Add files via upload

---
+++ Tugas1.java
@@ -0,0 +1,20 @@
+public class Tugas1 {
+
+    public static void main(String[] args){
+        System.out.println("\t\t\tPROVINSI JAWA TENGAH");
+        System.out.println("\t\t\tKABUPATEN PEKALONGAN");
+        System.out.println("\nNIK\t\t\t\t\t: 3326126410990001");
+        System.out.println("Nama\t\t\t\t: PRILYA LINTANG MARSHEILA");
+        System.out.println("Tempat/Tgl.Lahir\t: PEKALONGAN, 24-10-1999");
+        System.out.println("Jenis Kelamin\t\t: PEREMPUAN");
+        System.out.println("Alamat\t\t\t\t: PEGADEN TENGAH");
+        System.out.println("\tRT/RW\t\t\t: 003/001");
+        System.out.println("\tKel/Desa\t\t: PEGADEN TENGAH");
+        System.out.println("\tKecamatan\t\t: WONOPRINGGO");
+        System.out.println("Agama\t\t\t\t: ISLAM");
+        System.out.println("Satatus Perkawinan\t: BELUM KAWIN");
+        System.out.println("Pekerjaan\t\t\t: PELAJAR/MAHASISWA");
+        System.out.println("Kewarganegaraan\t\t: WNI");
+        System.out.println("Berlaku Hingga\t\t: SEUMUR HIDUP");
+    }
+}
---
+++ Tugas2.java
@@ -0,0 +1,15 @@
+public class Tugas2 {
+
+    public static void main(String[] args){
+        System.out.println("\t\t\tPROFIL MAHASISWA");
+        System.out.println("\t\t\t\tAKN KAJEN");
+        System.out.println("\nNIM\t\t\t\t: 171530022");
+        System.out.println("Nama\t\t\t: PRILYA LINTANG MARSHEILA");
+        System.out.println("Jenis Kelamin\t: PEREMPUAN");
+        System.out.println("Prodi\t\t\t: TEKNIK INFORMATIKA");
+        System.out.println("Jurusan\t\t\t: TEKNIK INFORMATIKA DAN KOMPUTER");
+        System.out.println("Tanggal Lahir\t: 24 Oktober 1999");
+        System.out.println("Alamat\t\t\t: PEGADEN TENGAH");
+        System.out.println("Agama\t\t\t: ISLAM");
+    }
+}
